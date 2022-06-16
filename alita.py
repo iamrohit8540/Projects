8abc5c9080444400974a20c8f1d4d375a50ed3e7
RO UNDERHOOD <strokeofdeath8540@gmail.com>
2022-06-16 09:24:54 +0530

    Create alita.py

---
+++ alita.py
@@ -0,0 +1,101 @@
+from json.tool import main
+from threading import main_thread
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+import datetime
+import wikipedia #pip install wikipedia
+import webbrowser
+import os
+import smtplib
+
+print("Initializing...")
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+print(voices[1].id)
+engine.setProperty('voice', voices[1].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def intro():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+
+    speak("I am alita; your new creation;I am here to help you")
+
+def inpcommand():
+         #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")  
+        return "None"
+    return query
+
+
+
+if __name__ == "__main__":
+    speak("Request initiated Mr. Rohit; We are launching in 3; 2; 1; ")
+    intro()
+
+    while True:
+    # if 1:
+        query = inpcommand().lower()
+
+        # Logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")   
+
+        elif 'play music' in query:
+            music_dir = 'C:\\Users\\iamro\\Music\\Music\\Heartbeats'
+            songs = os.listdir(music_dir)
+            print(songs)    
+            os.startfile(os.path.join(music_dir, songs[0]))
+
+        elif 'tell time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'open code' in query:
+            codePath = "C:\\Program Files\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+
+        elif 'bye' in query:
+            print("bye sir;see you soon")
+            speak("bye sir;see you soon")
+            exit()    
